42cf6ee9fb06c033b38acecf4a989ae60ac904c5
1531169@stud.hs-mannheim.de <sabakuno123>
2017-02-23 18:24:15 +0100

    solved problems with tooltip

--- CheckOnline/src/app/app.config.ts
+++ CheckOnline/src/app/app.config.ts
@@ -1,13 +1,77 @@
 export const APP_CONSTS = {
+    // navigation paths
     INDEX_PATH: '',
     LOGGED_IN_PATH: '/home',
     LOGGED_OUT_PATH: '/login',
+    LOGIN_PATH: '/login',
     CHAPTER_PATH: '/chapter',
     ACHIEVED_CHAPTER_PATH: '/achieved',
-    EDUCATIONAL_PLAN_PATH: 'educationalPlan',
-    CHANGE_AVATAR_PATH: 'changeavatar',
-    CHANGE_PASSWORD_PATH: 'changepw',
-    DELETE_PROFILE_PATH: 'deleteprofile',
+    EDUCATIONAL_PLAN_PATH: '/educationalPlan',
+    CHANGE_AVATAR_PATH: '/changeavatar',
+    CHANGE_PASSWORD_PATH: '/changepw',
+    DELETE_PROFILE_PATH: '/deleteprofile',
     NOT_FOUND_PATH: '**',
-    LOCALSTORAGE_TOKEN_ID: 'token'
+    LOCALSTORAGE_TOKEN_ID: 'token',
+    EMPTY_STRING: '',
+
+    // TOOLTIP SERVICE CONFIG
+    TOOLTIP_STYLE_TAG_ID: 'tooltipStyle',
+    TOOLTIP_COMPETENCE_CSS: `
+.tooltip {
+    z-index:500;
+}
+.tooltip .tooltip-inner {
+    background-color: #001a3a;
+    color: white;
+    text-align: left;
+    padding: 10px 15px;
+    width: 250px;
+    max-width: 250px;
+    min-width: 250px;
+    top: 200px;
+    /*
+    min-height: 60px;
+    transform: translateY(20%);
+    */
+    border: 3px solid white;
+}
+
+.tooltip.right .tooltip-arrow {
+    margin-top: -6px;
+    border-width: 6px 6px 6px 0;
+    border-right-color: white;
+}
+
+.tooltip.right {
+    margin-left: 12px;
+    padding: 0 6px;
+}`,
+    TOOLTIP_INPUT_CSS: `
+.tooltip {
+    z-index:500;
+}
+.tooltip .tooltip-inner {
+    background-color: #d3ddf2;
+    color: #001a3a;
+    text-align: left;
+    padding: 10px 15px;
+    width: 300px;
+    max-width: 300px;
+    min-width: 300px;
+    /*
+    margin-top: 50px;
+    height: 90px;
+    */
+}
+
+.tooltip.right .tooltip-arrow {
+    margin-top: -6px;
+    border-width: 6px 6px 6px 0;
+    border-right-color: #d3ddf2;
+}
+
+.tooltip.right {
+    margin-left: 5px;
+    padding: 0 6px;
+}`
 };
--- CheckOnline/src/app/services/tooltip.service.ts
+++ CheckOnline/src/app/services/tooltip.service.ts
@@ -1,99 +1,35 @@
 import {Injectable} from '@angular/core';
 import {Router} from "@angular/router";
 import {TooltipDirective} from "ng2-bootstrap";
+import {APP_CONSTS} from "../app.config";
 
 @Injectable()
 export class TooltipService {
     /*
-     Routes for input tooltip style
-     */
-    private routeLogin = "/";
-    private routeChangePw = "/changepw";
-    private routeDeleteProfile = "/deleteprofile";
-    /*
-     Routes for competences tooltip style
-     */
-    private routeHome = "/home";
-    private routeChapter = "/chapter";
-    private routeAchieved = "/achieved";
-    private routePlan = "/educationalPlan";
-    /*
      Information about the style, tag, css classes
      */
-    private styleTagId = "tooltipStyle";
     private styleTag: HTMLElement = null;
-    private competenceTooltipCSS = `
-.tooltip .tooltip-inner {
-    background-color: #001a3a;
-    color: white;
-    text-align: left;
-    padding: 10px 15px;
-    width: 250px;
-    max-width: 250px;
-    min-width: 250px;
-    top: 200px;
-    /*
-    min-height: 60px;
-    transform: translateY(20%);
-    */
-    border: 3px solid white;
-}
 
-.tooltip.right .tooltip-arrow {
-    margin-top: -6px;
-    border-width: 6px 6px 6px 0;
-    border-right-color: white;
-}
-
-.tooltip.right {
-    margin-left: 12px;
-    padding: 0 6px;
-}`;
-    private inputTooltipCSS = `
-.tooltip .tooltip-inner {
-    background-color: #d3ddf2;
-    color: #001a3a;
-    text-align: left;
-    padding: 10px 15px;
-    width: 300px;
-    max-width: 300px;
-    min-width: 300px;
-    /*
-    margin-top: 50px;
-    height: 90px;
-    */
-}
-
-.tooltip.right .tooltip-arrow {
-    margin-top: -6px;
-    border-width: 6px 6px 6px 0;
-    border-right-color: #d3ddf2;
-}
-
-.tooltip.right {
-    margin-left: 5px;
-    padding: 0 6px;
-}`;
     /*
      Selected tooltip style
      */
     private tooltipState: TooltipState = TooltipState.NONE;
 
     constructor(private router: Router) {
-        this.styleTag = document.getElementById(this.styleTagId);
+        this.styleTag = document.getElementById(APP_CONSTS.TOOLTIP_STYLE_TAG_ID);
         // react on route event to add the style automatically
         router.events.subscribe((e) => {
-            if (e.url.startsWith(this.routeChapter) || e.url.startsWith(this.routeAchieved)
-                || e.url.startsWith(this.routePlan) || e.url == this.routeHome) {
+            if (e.url.startsWith(APP_CONSTS.CHAPTER_PATH) || e.url.startsWith(APP_CONSTS.ACHIEVED_CHAPTER_PATH)
+                || e.url.startsWith(APP_CONSTS.EDUCATIONAL_PLAN_PATH)) {
                 if (this.tooltipState != TooltipState.COMPETENCE) {
                     this.tooltipState = TooltipState.COMPETENCE;
-                    this.styleTag.innerHTML = this.competenceTooltipCSS;
+                    this.styleTag.innerHTML = APP_CONSTS.TOOLTIP_COMPETENCE_CSS;
                 }
-            } else if (e.url == this.routeLogin || e.url == this.routeChangePw
-                || e.url == this.routeDeleteProfile) {
+            } else if (e.url == APP_CONSTS.LOGIN_PATH || e.url == APP_CONSTS.CHANGE_PASSWORD_PATH
+                || e.url == APP_CONSTS.DELETE_PROFILE_PATH) {
                 if (this.tooltipState != TooltipState.INPUT) {
                     this.tooltipState = TooltipState.INPUT;
-                    this.styleTag.innerHTML = this.inputTooltipCSS;
+                    this.styleTag.innerHTML = APP_CONSTS.TOOLTIP_INPUT_CSS;
                 }
             } else {
                 // nothing to change
@@ -107,7 +43,7 @@ export class TooltipService {
     }
 
     static hideTooltip(tooltipDirective: TooltipDirective) {
-        tooltipDirective.tooltip = "";
+        tooltipDirective.tooltip = APP_CONSTS.EMPTY_STRING;
         tooltipDirective.hide();
     }
 }
